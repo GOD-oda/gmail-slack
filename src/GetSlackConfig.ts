3a3ee2dad42883b83f19f6078d733b632703798d
GOD-oda <last.pass.oda@gmail.com>
2025-08-24 16:44:55 +0900

    Remove unused Slack rules in `GetSlackConfig.ts`
    Commented out or removed obsolete rule instances to streamline processing logic.

--- src/GetSlackConfig.ts
+++ src/GetSlackConfig.ts
@@ -16,49 +16,15 @@ export interface SlackConfig {
   channel: string;
 }
 
-const defaultConfig: SlackConfig = {
-  address: "",
-  icon_emoji: "",
-  channel: "",
-};
-
 export const getConfig = (gmailMessage: GmailMessage): SlackConfig | null => {
-  // TODO: AmericanexpressRule以外はgasでchannelを設定していないので動かない
   const rules: Rule[] = [
     new RakutenCard(gmailMessage),
-    // new ConnpassRule(gmailMessage),
-    // new AmazonRule(gmailMessage),
     new AmericanExpress(gmailMessage),
     new Vpass(gmailMessage),
-    // new GoogleRule(gmailMessage),
-    // new TripcomRule(gmailMessage),
-    // new JtbRule(gmailMessage),
     new Mufg(gmailMessage),
-    // new YoutrustRule(gmailMessage),
-    // new OffersRule(gmailMessage),
-    // new IssueRule(gmailMessage),
-    // new WantedlyRule(gmailMessage),
-    // new AgodaRule(gmailMessage),
-    // new FindyRule(gmailMessage),
-    // new AnaRule(gmailMessage),
     new Paypay(gmailMessage),
-    new Smbc(gmailMessage), // TODO: 同じSMBCだけどドメインが複数の場合に1つのルールクラスで対応するか検討する
-    // new SmbcMsgRule(gmailMessage),
-    // new SmartExRule(gmailMessage),
-    // new PearsonRule(gmailMessage),
-    // new FreeeRule(gmailMessage),
-    // new LaprasRule(gmailMessage),
-    // new MinkabuRule(gmailMessage),
-    // new ToyotaRentacarRule(gmailMessage),
-    // new ExpediaRule(gmailMessage),
-    // new SbiRule(gmailMessage),
-    // new TakarakujiRule(gmailMessage),
-    // new DevtoRule(gmailMessage),
-    // new DocomoCycleRule(gmailMessage),
-    // new HelloCyclingRule(gmailMessage),
+    new Smbc(gmailMessage),
     new Cloudflare(gmailMessage),
-    // new CredlyRule(gmailMessage),
-    // new UdemyRule(gmailMessage),
     new Aws(gmailMessage),
     new TokyoSuido(gmailMessage),
     new Mufgcard(gmailMessage),
